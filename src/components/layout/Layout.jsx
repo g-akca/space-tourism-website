409245a19fce2b0cbbca8989ddca988215d7c635
g-akca <gok.akca66@gmail.com>
2026-04-02 11:32:36 +0300

    Make the second div in HomePage take remaining height

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,11 @@
 import Header from "./Header";
 
-function Layout({ children, bg }) {
+function Layout({ children, bg="" }) {
   return (
-    <div className={`min-h-screen ${bg} bg-cover bg-bottom text-base leading-base text-blue-300`}>
+    <div className={`min-h-screen ${bg} bg-cover bg-bottom text-base leading-base text-blue-300 flex flex-col`}>
       <Header />
 
-      <main className="p-6">
+      <main className="p-6 grow flex flex-col">
         {children}
       </main>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
 function HomePage() {
   return (
-    <div className="text-center">
-      <p className="font-barlow-cond uppercase text-[16px] leading-[100%] tracking-[15%]">So, you want to travel to</p>
+    <div className="text-center grow flex flex-col">
+      <div className="flex flex-col gap-6">
+        <p className="font-barlow-cond uppercase text-[16px] leading-[100%] tracking-[15%]">So, you want to travel to</p>
+      </div>
+
+      <div className="grow">
+
+      </div>
     </div>
   )
 }
