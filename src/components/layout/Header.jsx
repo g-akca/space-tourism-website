70c0a221e392279ec37723650df25cbeb64e8f8c
g-akca <gok.akca66@gmail.com>
2026-04-02 10:57:31 +0300

    Add Header structure and styles

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,19 @@
+import logoImg from "/assets/shared/logo.svg";
+import hamburgerIcon from "/assets/shared/icon-hamburger.svg";
+
+import { Link } from "react-router";
+
 function Header() {
   return (
-    <>
-    </>
+    <header className="p-6 flex justify-between items-center">
+      <Link to="/">
+        <img src={logoImg} alt="Logo image" className="h-10" />
+      </Link>
+
+      <button type="button">
+        <img src={hamburgerIcon} alt="Menu icon" />
+      </button>
+    </header>
   )
 }
 
