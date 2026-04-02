752322fb4d7da46b43bbd07e20e4291b7dde7387
g-akca <gok.akca66@gmail.com>
2026-04-02 12:47:54 +0300

    Add border below nav link when page is active

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,32 +1,32 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 function NavBar() {
   return (
     <nav 
       className="
-        hidden tablet:flex items-center justify-end gap-12 
-        grow self-stretch bg-[rgba(255,255,255,0.05)] backdrop-blur-[80px] px-10 
+        hidden tablet:flex justify-end gap-12 px-10 
+        grow self-stretch bg-[rgba(255,255,255,0.05)] backdrop-blur-[80px] 
         uppercase text-white font-barlow-cond tracking-[2px] leading-auto
       "
     >
-      <Link to="/">
+      <NavLink to="/" end className={({ isActive }) => `flex items-center border-b-[3px] ${isActive ? "border-white" : "border-transparent"}`}>
         Home
-      </Link>
+      </NavLink>
 
-      <Link to="" className="flex gap-2">
+      <NavLink to="/destination" end className={({ isActive }) => `flex gap-2 items-center border-b-[3px] ${isActive ? "border-white" : "border-transparent"}`}>
         <strong className="tracking-[2.7px]">01</strong>
         <p>Destination</p>
-      </Link>
+      </NavLink>
 
-      <Link to="" className="flex gap-2">
+      <NavLink to="/crew" end className={({ isActive }) => `flex gap-2 items-center border-b-[3px] ${isActive ? "border-white" : "border-transparent"}`}>
         <strong className="tracking-[2.7px]">02</strong>
         <p>Crew</p>
-      </Link>
+      </NavLink>
 
-      <Link to="" className="flex gap-2">
+      <NavLink to="/technology" end className={({ isActive }) => `flex gap-2 items-center border-b-[3px] ${isActive ? "border-white" : "border-transparent"}`}>
         <strong className="tracking-[2.7px]">03</strong>
         <p>Technology</p>
-      </Link>
+      </NavLink>
     </nav>
   )
 }
