bf86d77935cd1f33ec5050ee1593e0ccdbb76c8d
g-akca <gok.akca66@gmail.com>
2026-04-02 11:15:24 +0300

    Add Barlow Condensed font, change base font size

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 function Layout({ children, bg }) {
   return (
-    <div className={`min-h-screen ${bg} bg-cover text-base leading-base`}>
+    <div className={`min-h-screen ${bg} bg-cover text-base leading-base text-blue-300`}>
       <Header />
 
       <main className="p-6">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
 function HomePage() {
   return (
-    <>
-    </>
+    <div className="text-center">
+      <p className="font-barlow-cond uppercase text-[16px] leading-[100%] tracking-[15%]">So, you want to travel to</p>
+    </div>
   )
 }
 
