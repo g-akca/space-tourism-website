35ca810b4c456da7b6f0b80272df56fae9c59d9a
g-akca <gok.akca66@gmail.com>
2026-04-02 12:20:22 +0300

    Add tablet responsive Header styles and navbar

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,14 +5,35 @@ import { Link } from "react-router";
 
 function Header() {
   return (
-    <header className="p-6 flex justify-between items-center">
-      <Link to="/">
-        <img src={logoImg} alt="Logo image" className="h-10" />
+    <header className="p-6 tablet:p-0 flex justify-between items-center">
+      <Link to="/" className="tablet:py-6 tablet:px-10">
+        <img src={logoImg} alt="Logo image" className="h-10 tablet:h-12" />
       </Link>
 
-      <button type="button" className="cursor-pointer">
+      <button type="button" className="cursor-pointer tablet:hidden">
         <img src={hamburgerIcon} alt="Menu icon" />
       </button>
+
+      <nav className="hidden tablet:flex items-center justify-end gap-12 grow self-stretch bg-[rgba(255,255,255,0.05)] backdrop-blur-[80px] px-10 uppercase text-white font-barlow-cond tracking-[2px] leading-auto">
+        <Link to="/">
+          Home
+        </Link>
+
+        <Link to="" className="flex gap-2">
+          <strong className="tracking-[2.7px]">01</strong>
+          <p>Destination</p>
+        </Link>
+
+        <Link to="" className="flex gap-2">
+          <strong className="tracking-[2.7px]">02</strong>
+          <p>Crew</p>
+        </Link>
+
+        <Link to="" className="flex gap-2">
+          <strong className="tracking-[2.7px]">03</strong>
+          <p>Technology</p>
+        </Link>
+      </nav>
     </header>
   )
 }
