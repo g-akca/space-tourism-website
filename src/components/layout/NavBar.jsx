be5aad5073e9d01bb903d7a1089f43005d031248
g-akca <gok.akca66@gmail.com>
2026-04-02 12:49:45 +0300

    Create reusable navClass constant in NavBar

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,6 +1,11 @@
 import { NavLink } from "react-router";
 
 function NavBar() {
+  const navClass = ({ isActive }) => 
+    `flex items-center gap-2 border-b-[3px] ${
+      isActive ? "border-white" : "border-transparent"
+    }`;
+
   return (
     <nav 
       className="
@@ -9,21 +14,21 @@ function NavBar() {
         uppercase text-white font-barlow-cond tracking-[2px] leading-auto
       "
     >
-      <NavLink to="/" end className={({ isActive }) => `flex items-center border-b-[3px] ${isActive ? "border-white" : "border-transparent"}`}>
+      <NavLink to="/" end className={navClass}>
         Home
       </NavLink>
 
-      <NavLink to="/destination" end className={({ isActive }) => `flex gap-2 items-center border-b-[3px] ${isActive ? "border-white" : "border-transparent"}`}>
+      <NavLink to="/destination" end className={navClass}>
         <strong className="tracking-[2.7px]">01</strong>
         <p>Destination</p>
       </NavLink>
 
-      <NavLink to="/crew" end className={({ isActive }) => `flex gap-2 items-center border-b-[3px] ${isActive ? "border-white" : "border-transparent"}`}>
+      <NavLink to="/crew" end className={navClass}>
         <strong className="tracking-[2.7px]">02</strong>
         <p>Crew</p>
       </NavLink>
 
-      <NavLink to="/technology" end className={({ isActive }) => `flex gap-2 items-center border-b-[3px] ${isActive ? "border-white" : "border-transparent"}`}>
+      <NavLink to="/technology" end className={navClass}>
         <strong className="tracking-[2.7px]">03</strong>
         <p>Technology</p>
       </NavLink>
