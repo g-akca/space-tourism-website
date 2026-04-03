5ea0009aa3fa1010c4814ffb794d27c5ece92eab
g-akca <gok.akca66@gmail.com>
2026-04-03 19:37:56 +0300

    Hide menu on tablet and desktop sizes

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -4,7 +4,7 @@ import closeIcon from "/assets/shared/icon-close.svg";
 
 function Menu({ closeMenu }) {
   return (
-    <div className="fixed top-0 right-0 z-50 h-screen w-63.5 pl-8 bg-[rgba(11,13,23,0.15)] backdrop-blur-[80px] flex flex-col gap-12">
+    <div className="tablet:hidden fixed top-0 right-0 z-50 h-screen w-63.5 pl-8 bg-[rgba(11,13,23,0.15)] backdrop-blur-[80px] flex flex-col gap-12">
       <div className="py-8 px-6 flex justify-end">
         <button type="button" className="cursor-pointer" onClick={closeMenu}>
           <img src={closeIcon} alt="Close menu" />
