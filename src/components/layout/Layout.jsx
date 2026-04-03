33440af0ab13baef24e1042166a4348187d51414
g-akca <gok.akca66@gmail.com>
2026-04-03 15:57:35 +0300

    Add desktop responsive HomePage styles

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,7 +5,7 @@ function Layout({ children, bg = "" }) {
     <div className={`min-h-screen ${bg} bg-cover bg-bottom text-base tablet:text-[16px] desktop:text-[18px] leading-base text-blue-300 flex flex-col`}>
       <Header />
 
-      <main className="p-6 tablet:p-10 grow flex flex-col">
+      <main className="p-6 tablet:p-10 grow flex flex-col items-center">
         {children}
       </main>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,17 +2,19 @@ import CircularButton from "../components/ui/CircularButton";
 
 function HomePage() {
   return (
-    <div className="text-center grow flex flex-col items-center tablet:py-22">
-      <div className="flex flex-col gap-6 tablet:w-lg">
-        <p className="font-barlow-cond uppercase text-[16px] tablet:text-[28px] leading-auto tracking-[15%] tablet:tracking-[4px]">So, you want to travel to</p>
-        <h1 className="font-bellefair uppercase text-[80px] tablet:text-[144px] leading-auto text-white">Space</h1>
-        <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
-      </div>
+    <div className="grow flex flex-col tablet:py-22 desktop:justify-end">
+      <div className="text-center grow flex flex-col items-center desktop:w-277.5 desktop:text-start desktop:grow-0 desktop:grid desktop:grid-cols-2 desktop:gap-7.5">
+        <div className="flex flex-col gap-6 tablet:w-lg desktop:w-auto">
+          <p className="font-barlow-cond uppercase text-[16px] tablet:text-[28px] leading-auto tracking-[15%] tablet:tracking-[4px]">So, you want to travel to</p>
+          <h1 className="font-bellefair uppercase text-[80px] tablet:text-[144px] leading-auto text-white">Space</h1>
+          <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+        </div>
 
-      <div className="grow flex justify-center items-center p-8">
-        <CircularButton className="w-36 tablet:w-68 aspect-square">
-          Explore
-        </CircularButton>
+        <div className="grow flex justify-center items-center py-8 desktop:justify-self-end">
+          <CircularButton className="w-36 tablet:w-68 aspect-square">
+            Explore
+          </CircularButton>
+        </div>
       </div>
     </div>
   )
