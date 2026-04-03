9285a3a2a542204db9119b259fc1c435e0e2f3db
g-akca <gok.akca66@gmail.com>
2026-04-03 16:03:15 +0300

    Add menu state and create Menu component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,15 @@
 import logoImg from "/assets/shared/logo.svg";
 import hamburgerIcon from "/assets/shared/icon-hamburger.svg";
 
+import { useState } from "react";
 import { Link } from "react-router";
 
 import NavBar from "./NavBar";
+import Menu from "./Menu";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className="p-6 tablet:p-0 desktop:pt-10 flex justify-between items-center">
       <Link to="/" className="tablet:py-6 tablet:px-10 desktop:px-16">
@@ -14,11 +18,15 @@ function Header() {
 
       <div className="hidden desktop:block grow h-px bg-[#979797] opacity-25 -mr-8 z-10" />
 
-      <button type="button" className="cursor-pointer tablet:hidden">
+      <button type="button" className="cursor-pointer tablet:hidden" onClick={() => setIsMenuOpen(true)}>
         <img src={hamburgerIcon} alt="Menu icon" />
       </button>
 
       <NavBar />
+
+      {isMenuOpen && (
+        <Menu />
+      )}
     </header>
   )
 }
