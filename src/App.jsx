e41ec782f932bb8604a7329fac2ba54f553d0e84
g-akca <gok.akca66@gmail.com>
2026-04-04 12:55:14 +0300

    Add desktop bg to CrewPage

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       <Route
         path="/crew"
         element={
-          <Layout bg="bg-[url('assets/crew/background-crew-mobile.jpg')] tablet:bg-[url('assets/crew/background-crew-tablet.jpg')]">
+          <Layout bg="bg-[url('assets/crew/background-crew-mobile.jpg')] tablet:bg-[url('assets/crew/background-crew-tablet.jpg')] desktop:bg-[url('assets/crew/background-crew-desktop.jpg')]">
             <CrewPage />
           </Layout>
         }
