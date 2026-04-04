b858fa8b0e1712d147be28438d3815414151e53b
g-akca <gok.akca66@gmail.com>
2026-04-04 12:11:22 +0300

    Fetch CrewPage content based on selected crew

--- src/pages/CrewPage.jsx
+++ src/pages/CrewPage.jsx
@@ -1,6 +1,11 @@
-import douglasImg from "/assets/crew/image-douglas-hurley.png";
+import data from "../data/data.json";
+
+import { useState } from "react";
 
 function CrewPage() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const crew = data.crew[selectedIndex];
+
   return (
     <div className="grow flex flex-col gap-6 text-center">
       <div className="flex justify-center gap-6 uppercase font-barlow-cond text-[16px] leading-auto tracking-[15%] text-white">
@@ -12,22 +17,26 @@ function CrewPage() {
         <div className="pt-10 flex flex-col gap-6">
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2 uppercase font-bellefair text-white">
-              <p className="opacity-50 text-[18px]">Commander</p>
-              <p className="text-[24px]">Douglas Hurley</p>
+              <p className="opacity-50 text-[18px]">{crew.role}</p>
+              <p className="text-[24px]">{crew.name}</p>
             </div>
 
-            <p className="h-41">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+            <p className="h-41">{crew.bio}</p>
           </div>
 
           <div className="flex justify-center gap-4">
-            <button type="button" className="h-2.5 aspect-square rounded-full bg-white" />
-            <button type="button" className="h-2.5 aspect-square rounded-full bg-white opacity-20" />
-            <button type="button" className="h-2.5 aspect-square rounded-full bg-white opacity-20" />
-            <button type="button" className="h-2.5 aspect-square rounded-full bg-white opacity-20" />
+            {data.crew.map((item, index) => (
+              <button
+                key={item.name}
+                type="button"
+                onClick={() => setSelectedIndex(index)}
+                className={`h-2.5 bg-white aspect-square rounded-full ${selectedIndex === index ? "" : "opacity-20"}`}
+              />
+            ))}
           </div>
         </div>
 
-        <img src={douglasImg} alt="Image of Douglas Hurley" className="h-85 mask-[linear-gradient(to_bottom,black_85%,transparent)]" />
+        <img src={crew.images.png} alt={`Image of ${crew.name}`} className="h-85 mask-[linear-gradient(to_bottom,black_85%,transparent)]" />
       </div>
     </div>
   )
