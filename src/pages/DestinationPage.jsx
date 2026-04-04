924fd2a71b3725e104bd74e682c673e9c4ffa463
g-akca <gok.akca66@gmail.com>
2026-04-04 10:17:20 +0300

    Fetch DestinationPage content based on selected destination

--- src/pages/DestinationPage.jsx
+++ src/pages/DestinationPage.jsx
@@ -1,6 +1,11 @@
-import moonImg from "/assets/destination/image-moon.png";
+import data from "../data/data.json";
+
+import { useState } from "react";
 
 function DestinationPage() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const destination = data.destinations[selectedIndex];
+
   return (
     <div className="grow flex flex-col gap-6 text-center">
       <div className="flex justify-center gap-6 uppercase font-barlow-cond text-[16px] leading-auto tracking-[15%] text-white">
@@ -9,38 +14,36 @@ function DestinationPage() {
       </div>
       
       <div className="flex flex-col gap-8 items-center">
-        <img src={moonImg} alt="Moon image" className="w-37.5 aspect-square my-7" />
+        <img src={destination.images.png} alt={`${destination.name} image`} className="w-37.5 aspect-square my-7" />
 
         <div className="flex flex-col gap-6">
           <div className="flex justify-center gap-8 h-8 font-barlow-cond text-[14px] leading-auto tracking-[15%]">
-            <button type="button" className="flex items-start relative after:absolute after:w-full after:h-0.75 after:bg-white after:-left-px after:bottom-0">
-              <p className="uppercase">Moon</p>
-            </button>
-            <button type="button" className="flex items-start relative">
-              <p className="uppercase">Mars</p>
-            </button>
-            <button type="button" className="flex items-start relative">
-              <p className="uppercase">Europa</p>
-            </button>
-            <button type="button" className="flex items-start relative">
-              <p className="uppercase">Titan</p>
-            </button>
+            {data.destinations.map((item, index) => (
+              <button
+                key={item.name}
+                type="button"
+                onClick={() => setSelectedIndex(index)}
+                className={`flex items-start relative ${selectedIndex === index ? "after:absolute after:w-full after:h-0.75 after:bg-white after:left-0 after:bottom-0" : ""}`}
+              >
+                <p className="uppercase">{item.name}</p>
+              </button>
+            ))}
           </div>
 
           <div className="pb-6 border-b border-[#979797] flex flex-col gap-4">
-            <h2 className="font-bellefair text-white uppercase text-[56px] leading-auto">Moon</h2>
-            <p className="text-blue-300"> See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+            <h2 className="font-bellefair text-white uppercase text-[56px] leading-auto">{destination.name}</h2>
+            <p className="text-blue-300 h-34">{destination.description}</p>
           </div>
 
           <div className="flex flex-col gap-6 uppercase">
             <div className="flex flex-col items-center gap-3">
               <p className="text-blue-300 text-[14px] leading-auto font-barlow-cond tracking-[2px]">Avg. Distance</p>
-              <p className="text-white text-[28px] leading-auto font-bellefair">384,400 km</p>
+              <p className="text-white text-[28px] leading-auto font-bellefair">{destination.distance}</p>
             </div>
 
             <div className="flex flex-col items-center gap-3">
               <p className="text-blue-300 text-[14px] leading-auto font-barlow-cond tracking-[2px]">Est. Travel Time</p>
-              <p className="text-white text-[28px] leading-auto font-bellefair">3 Days</p>
+              <p className="text-white text-[28px] leading-auto font-bellefair">{destination.travel}</p>
             </div>
           </div>
         </div>
