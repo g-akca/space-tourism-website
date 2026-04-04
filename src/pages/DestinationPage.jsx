0ff4af2ee4bcadf4ea866268f39e8628971fb718
g-akca <gok.akca66@gmail.com>
2026-04-04 10:31:48 +0300

    Add tablet responsive DestinationPage styles

--- src/pages/DestinationPage.jsx
+++ src/pages/DestinationPage.jsx
@@ -7,17 +7,17 @@ function DestinationPage() {
   const destination = data.destinations[selectedIndex];
 
   return (
-    <div className="grow flex flex-col gap-6 text-center">
-      <div className="flex justify-center gap-6 uppercase font-barlow-cond text-[16px] leading-auto tracking-[15%] text-white">
+    <div className="grow w-full flex flex-col gap-6 text-center">
+      <div className="flex justify-center tablet:justify-start gap-6 uppercase font-barlow-cond text-[16px] tablet:text-[20px] leading-auto tracking-[15%] text-white">
         <p className="font-bold opacity-25">01</p>
         <p>Pick your destination</p>
       </div>
       
       <div className="flex flex-col gap-8 items-center">
-        <img src={destination.images.png} alt={`${destination.name} image`} className="w-37.5 aspect-square my-7" />
+        <img src={destination.images.png} alt={`${destination.name} image`} className="w-37.5 tablet:w-75 aspect-square my-7" />
 
-        <div className="flex flex-col gap-6">
-          <div className="flex justify-center gap-8 h-8 font-barlow-cond text-[14px] leading-auto tracking-[15%]">
+        <div className="flex flex-col gap-6 tablet:max-w-128.5">
+          <div className="flex justify-center gap-8 h-8 font-barlow-cond text-[14px] tablet:text-[16px] leading-auto tracking-[15%] tablet:tracking-[2px]">
             {data.destinations.map((item, index) => (
               <button
                 key={item.name}
@@ -31,11 +31,11 @@ function DestinationPage() {
           </div>
 
           <div className="pb-6 border-b border-[#979797] flex flex-col gap-4">
-            <h2 className="font-bellefair text-white uppercase text-[56px] leading-auto">{destination.name}</h2>
-            <p className="text-blue-300 h-34">{destination.description}</p>
+            <h2 className="font-bellefair text-white uppercase text-[56px] tablet:text-[80px] leading-auto">{destination.name}</h2>
+            <p className="text-blue-300 tablet:text-[16px] h-34 tablet:h-auto">{destination.description}</p>
           </div>
 
-          <div className="flex flex-col gap-6 uppercase">
+          <div className="flex flex-col tablet:grid tablet:grid-cols-2 tablet:justify-items-center gap-6 uppercase">
             <div className="flex flex-col items-center gap-3">
               <p className="text-blue-300 text-[14px] leading-auto font-barlow-cond tracking-[2px]">Avg. Distance</p>
               <p className="text-white text-[28px] leading-auto font-bellefair">{destination.distance}</p>
