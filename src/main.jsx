405faf5abbc4d78547d725b7e7078ee6e84abde5
g-akca <gok.akca66@gmail.com>
2026-04-07 17:10:00 +0300

    Remove basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/space-tourism-website/">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
